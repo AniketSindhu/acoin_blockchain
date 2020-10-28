1ea8eb541b6d5b18952c41571715226d0fdd7b0b
Aniket Sindhu <sindhuaniket@gmail.com>
2020-10-28 23:21:26 +0530

    getBalance call done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:acoin_blockchain/slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:web3dart/web3dart.dart';
@@ -37,18 +38,47 @@ class _MyHomePageState extends State<MyHomePage> {
   bool data = false;
   int amount = 0;
   final myAddress = "0x830748d372c6C6BEBb5BF122f6803f9868fe6c93";
+  var myData;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     httpClient = Client();
-    ethClient = Web3Client('https://rinkeby.infura.io/v3/79a71b288cea4e2ea6e41389d8c7b46f', httpClient);
+    ethClient = Web3Client(
+        'https://rinkeby.infura.io/v3/79a71b288cea4e2ea6e41389d8c7b46f',
+        httpClient);
     getBalance(myAddress);
   }
 
-  Future<void> getBalance(String targetAddress) async{
-    EthereumAddress address = EthereumAddress.fromHex(targetAddress);
+  Future<DeployedContract> loadContract() async {
+    String abi = await rootBundle.loadString("assets/abi.json");
+    String contractAddress = "0x9704e539D741e5C59E29FaFBe6b4Ab271bd68FC1";
+
+    final contract = DeployedContract(ContractAbi.fromJson(abi, "Acoin"),
+        EthereumAddress.fromHex(contractAddress));
+
+    return contract;
   }
+
+  Future<List<dynamic>> query(String functionName, List<dynamic> args) async {
+    final contract = await loadContract();
+    final ethFunction = contract.function(functionName);
+    final result = await ethClient.call(
+        contract: contract, function: ethFunction, params: args);
+
+    return result;
+  }
+
+  Future<void> getBalance(String targetAddress) async {
+    //EthereumAddress address = EthereumAddress.fromHex(targetAddress);
+    List<dynamic> result = await query("getBalnce",[]);
+
+    myData=result[0];
+    data=true;
+    setState(() {
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,47 +97,48 @@ class _MyHomePageState extends State<MyHomePage> {
               "Balance".text.gray700.xl2.semiBold.makeCentered(),
               10.heightBox,
               data
-                  ? "\$1".text.bold.xl6.makeCentered()
+                  ? "\$$myData".text.bold.xl6.makeCentered().shimmer()
                   : CircularProgressIndicator().centered()
             ]))
                 .p16
                 .white
-                .size(context.screenWidth, context.percentHeight * 18)
+                .size(context.screenWidth, context.percentHeight * 20)
                 .rounded
                 .shadowXl
                 .make()
                 .p16(),
             30.heightBox,
-
             SliderWidget(
-              min:0,
-              max:100,
-              finalVal: (val){
-                amount= (val*100).round();
+              min: 0,
+              max: 100,
+              finalVal: (val) {
+                amount = (val * 100).round();
               },
-              ).centered(),
-
+            ).centered(),
             HStack(
               [
                 FlatButton.icon(
-                    onPressed: () {},
-                    color: Colors.blue,
-                    shape: Vx.roundedSm,
-                    icon: Icon(Icons.refresh, color: Colors.white),
-                    label: "Refresh".text.white.make()).h(50),
+                        onPressed: ()=>getBalance(myAddress),
+                        color: Colors.blue,
+                        shape: Vx.roundedSm,
+                        icon: Icon(Icons.refresh, color: Colors.white),
+                        label: "Refresh".text.white.make())
+                    .h(50),
                 FlatButton.icon(
-                    onPressed: () {},
-                    color: Colors.green,
-                    shape: Vx.roundedSm,
-                    icon: Icon(Icons.call_made_outlined, color: Colors.white),
-                    label: "Deposit".text.white.make()).h(50),
+                        onPressed: () {},
+                        color: Colors.green,
+                        shape: Vx.roundedSm,
+                        icon:
+                            Icon(Icons.call_made_outlined, color: Colors.white),
+                        label: "Deposit".text.white.make())
+                    .h(50),
                 FlatButton.icon(
-                    onPressed: () {},
-                    color: Colors.red,
-                    shape: Vx.roundedSm,
-                    icon:
-                        Icon(Icons.call_received, color: Colors.white),
-                    label: "Withdraw".text.white.make()).h(50),
+                        onPressed: () {},
+                        color: Colors.red,
+                        shape: Vx.roundedSm,
+                        icon: Icon(Icons.call_received, color: Colors.white),
+                        label: "Withdraw".text.white.make())
+                    .h(50),
               ],
               alignment: MainAxisAlignment.spaceAround,
               axisSize: MainAxisSize.max,
